9ba6dd9229d8b38de0ef45b921f1aa5094997d5d
kunduz <kunduznbkva@gmail.com>
2021-05-22 16:27:20 +0600

    mmm

--- app/src/main/java/kg/tabiyat/ui/main/profile/AccountFragment.kt
+++ app/src/main/java/kg/tabiyat/ui/main/profile/AccountFragment.kt
@@ -1,21 +1,28 @@
 package kg.tabiyat.ui.main.profile
 
+import android.Manifest
 import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import kg.tabiyat.R
 import kg.tabiyat.base.URIPathHelper
 import kg.tabiyat.base.loadImage
 import kg.tabiyat.base.showToastShort
 import kg.tabiyat.data.model.Customer
 import kg.tabiyat.databinding.AccountFragmentBinding
+import kg.tabiyat.ui.main.map.MapsFragment
 import kg.tabiyat.ui.main.profile.viewModels.AccountViewModel
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
@@ -23,6 +30,7 @@ import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.asRequestBody
 import org.koin.android.ext.android.inject
 import java.io.File
+
 
 
 class AccountFragment : Fragment() {
@@ -33,6 +41,8 @@ class AccountFragment : Fragment() {
     private var name: String = String()
     private lateinit var file: File
     private lateinit var body: MultipartBody.Part
+    private var galleryPermissionGranted = false
+
 
 
     companion object {
@@ -62,6 +72,11 @@ class AccountFragment : Fragment() {
             name = binding.accountName.text.toString()
             viewModel.updateUserAvatar(body)
             viewModel.updateUserName(name)
+            customer.avatar = body
+            customer.fullName = name
+            val bundle = Bundle()
+            bundle.putSerializable("edited_customer",customer)
+            Navigation.findNavController(it).navigate(R.id.action_accountFragment_to_navigation_profile)
         }
     }
 
@@ -85,7 +100,6 @@ class AccountFragment : Fragment() {
             val data: Intent? = result.data
             imageUri = data?.data
             binding.accountAvatar.loadImage(imageUri.toString())
-
             val uriPathHelper = URIPathHelper()
             val filePath = uriPathHelper.getPath(requireContext(), imageUri!!) // create RequestBody instance from file
 
@@ -104,9 +118,46 @@ class AccountFragment : Fragment() {
 
     private fun changeAvatar() {
         binding.accountChangeAvatar.setOnClickListener {
-            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
-            resultLauncher.launch(gallery)
+            getGalleryPermission()
+            if(!galleryPermissionGranted){
+                val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+                resultLauncher.launch(gallery)
+            } else if(galleryPermissionGranted){
+                requireContext().showToastShort(getString(R.string.grant_permission))
+            }
+            }
         }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            100 -> {
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                 Log.e("Permission","Permission is granted")
+                } else requireContext().showToastShort(getString(R.string.grant_permission))
+            }
+        }
+    }
+
+    private fun getGalleryPermission() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                galleryPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(), arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+               100
+            )
+        }
+    }
+
+
     }
 
 
@@ -114,4 +165,6 @@ class AccountFragment : Fragment() {
 
 
 
-}
+
+
+
